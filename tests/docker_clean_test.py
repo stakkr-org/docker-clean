9052da18e1a97f78dcf828dac6dc606d0aac1e51
Emmanuel Dyan <emmanueldyan@gmail.com>
2019-07-07 10:39:29 +0200

    added more tests for more coverage

--- tests/docker_clean_test.py
+++ tests/docker_clean_test.py
@@ -1,8 +1,9 @@
+import io
 import os
 import sys
 import unittest
-
 import docker_clean
+from contextlib import redirect_stdout
 from click.testing import CliRunner
 from docker.errors import NotFound
 from docker import client
@@ -12,38 +13,142 @@ sys.path.insert(0, BASE_DIR + '/../')
 
 
 class DockerCleanTest(unittest.TestCase):
-    def test_pretend_remove_containers(self):
-        docker_clean.remove_containers(False)
+    def test_remove_containers_empty(self):
+        clean_cts()
 
-    def test_pretend_remove_images(self):
-        docker_clean.remove_images(False)
+        fake_file = io.StringIO()
+        with redirect_stdout(fake_file):
+            docker_clean.remove_containers(True)
+        res = fake_file.getvalue()
+        self.assertEqual('No exited container to remove', res.strip())
 
-    def test_pretend_remove_networks(self):
+    def test_remove_containers_pretend(self):
+        clean_cts()
+        clean_images()
+
+        DOCKER_CLIENT.containers.run('hello-world', remove=False)
+        num_containers = len(DOCKER_CLIENT.containers.list(all=True))
+        fake_file = io.StringIO()
+        with redirect_stdout(fake_file):
+            docker_clean.remove_containers(False)
+        res = fake_file.getvalue()
+        self.assertEqual("--force is not set so I won't do anything", res.strip())
+        self.assertEqual(num_containers, len(DOCKER_CLIENT.containers.list(all=True)))
+
+    def test_remove_containers(self):
+        clean_cts()
+        clean_images()
+
+        DOCKER_CLIENT.containers.run('hello-world', remove=False)
+        num_containers = len(DOCKER_CLIENT.containers.list(all=True))
+        fake_file = io.StringIO()
+        with redirect_stdout(fake_file):
+            docker_clean.remove_containers(True)
+        res = fake_file.getvalue()
+        self.assertEqual("Removed 1 exited container(s), saved", res[:38].strip())
+        self.assertEqual((num_containers - 1), len(DOCKER_CLIENT.containers.list(all=True)))
+
+    def test_remove_images_empty(self):
+        clean_cts()
+        clean_images()
+
+        fake_file = io.StringIO()
+        with redirect_stdout(fake_file):
+            docker_clean.remove_images(True)
+        res = fake_file.getvalue()
+        self.assertEqual('No image to remove', res.strip())
+
+    def test_remove_images_pretend(self):
+        clean_cts()
+        clean_images()
+
+        DOCKER_CLIENT.containers.run('hello-world', remove=True)
+        num_images = len(DOCKER_CLIENT.images.list(all=True))
+        fake_file = io.StringIO()
+        with redirect_stdout(fake_file):
+            docker_clean.remove_images(False)
+        res = fake_file.getvalue()
+        self.assertEqual("--force is not set so I won't do anything", res.strip())
+        self.assertEqual(num_images, len(DOCKER_CLIENT.images.list(all=True)))
+
+    def test_remove_images(self):
+        clean_cts()
+        clean_images()
+
+        DOCKER_CLIENT.containers.run('hello-world', remove=True)
+        num_images = len(DOCKER_CLIENT.images.list(all=True))
+        fake_file = io.StringIO()
+        with redirect_stdout(fake_file):
+            docker_clean.remove_images(True)
+        res = fake_file.getvalue()
+        self.assertEqual((num_images - 1), len(DOCKER_CLIENT.images.list(all=True)))
+        self.assertEqual("Removed", res[:9].strip())
+
+    def test_remove_networks_empty(self):
+        clean_cts()
+        DOCKER_CLIENT.networks.prune()
+
+        fake_file = io.StringIO()
+        with redirect_stdout(fake_file):
+            docker_clean.remove_networks(True)
+        res = fake_file.getvalue()
+        self.assertEqual('No network to remove', res.strip())
+
+    def test_remove_networks_pretend(self):
         DOCKER_CLIENT.networks.prune()
         DOCKER_CLIENT.networks.create('network_pytest')
+
         num_networks = len(DOCKER_CLIENT.networks.list())
-        docker_clean.remove_networks(False)
+        fake_file = io.StringIO()
+        with redirect_stdout(fake_file):
+            docker_clean.remove_networks(False)
+        res = fake_file.getvalue()
+        self.assertEqual("--force is not set so I won't do anything", res.strip())
         self.assertEqual(num_networks, len(DOCKER_CLIENT.networks.list()))
 
     def test_remove_networks(self):
         DOCKER_CLIENT.networks.prune()
         DOCKER_CLIENT.networks.create('network_pytest')
+
         num_networks = len(DOCKER_CLIENT.networks.list())
-        docker_clean.remove_networks(True)
+        fake_file = io.StringIO()
+        with redirect_stdout(fake_file):
+            docker_clean.remove_networks(True)
+        res = fake_file.getvalue()
+        self.assertEqual("Removed 1 network(s)", res.strip())
         self.assertEqual((num_networks - 1), len(DOCKER_CLIENT.networks.list()))
 
-    def test_pretend_remove_volumes(self):
+    def test_remove_volumes_empty(self):
+        clean_cts()
+        DOCKER_CLIENT.volumes.prune()
+
+        fake_file = io.StringIO()
+        with redirect_stdout(fake_file):
+            docker_clean.remove_volumes(True)
+        res = fake_file.getvalue()
+        self.assertEqual('No volume to remove', res.strip())
+
+    def test_remove_volumes_pretend(self):
         DOCKER_CLIENT.volumes.prune()
         DOCKER_CLIENT.volumes.create('test123')
+
         num_volumes = len(DOCKER_CLIENT.volumes.list())
-        docker_clean.remove_volumes(False)
+        fake_file = io.StringIO()
+        with redirect_stdout(fake_file):
+            docker_clean.remove_volumes(False)
+        res = fake_file.getvalue()
+        self.assertEqual("--force is not set so I won't do anything", res.strip())
         self.assertEqual(num_volumes, len(DOCKER_CLIENT.volumes.list()))
 
     def test_remove_volumes(self):
         DOCKER_CLIENT.volumes.prune()
         DOCKER_CLIENT.volumes.create('test123')
         num_volumes = len(DOCKER_CLIENT.volumes.list())
-        docker_clean.remove_volumes(True)
+        fake_file = io.StringIO()
+        with redirect_stdout(fake_file):
+            docker_clean.remove_volumes(True)
+        res = fake_file.getvalue()
+        self.assertEqual("Removed 1 volume(s), saved", res[:28].strip())
         self.assertEqual((num_volumes - 1), len(DOCKER_CLIENT.volumes.list()))
 
     def test_no_arg(self):
@@ -76,8 +181,8 @@ class DockerCleanTest(unittest.TestCase):
 
         # Standard info
         num_default_nets = len(DOCKER_CLIENT.networks.list())
-        num_default_cts = len(DOCKER_CLIENT.containers.list())
-        num_default_images = len(DOCKER_CLIENT.images.list())
+        num_default_cts = len(DOCKER_CLIENT.containers.list(all=True))
+        num_default_images = len(DOCKER_CLIENT.images.list(all=True))
         num_default_vols = len(DOCKER_CLIENT.volumes.list())
 
         # Start specific networks
@@ -100,7 +205,7 @@ class DockerCleanTest(unittest.TestCase):
         net_pytest.connect(ct_test)
 
         # Make sure we have two new image : hello-world + adminer
-        num_images = len(DOCKER_CLIENT.images.list())
+        num_images = len(DOCKER_CLIENT.images.list(all=True))
         self.assertIs(num_images, (2 + num_default_images))
 
         # Make sure we have two new containers
@@ -131,7 +236,7 @@ class DockerCleanTest(unittest.TestCase):
         # Except ct_test that is running so : 1 image, 1 network, 1 container
         self.assertIs(len(DOCKER_CLIENT.networks.list()), num_default_nets + 1)
         self.assertIs(len(DOCKER_CLIENT.volumes.list()), num_default_vols)
-        self.assertIs(len(DOCKER_CLIENT.images.list()), num_default_images + 1)
+        self.assertIs(len(DOCKER_CLIENT.images.list(all=True)), num_default_images + 1)
         self.assertIs(len(DOCKER_CLIENT.networks.list()), num_default_nets + 1)
 
         ct_test.stop()
@@ -160,7 +265,7 @@ class DockerCleanTest(unittest.TestCase):
         # Except ct_test so : 1 image, 1 network, 1 container
         self.assertIs(len(DOCKER_CLIENT.networks.list()), num_default_nets)
         self.assertIs(len(DOCKER_CLIENT.volumes.list()), num_default_vols)
-        self.assertIs(len(DOCKER_CLIENT.images.list()), num_default_images)
+        self.assertIs(len(DOCKER_CLIENT.images.list(all=True)), num_default_images)
         self.assertIs(len(DOCKER_CLIENT.networks.list()), num_default_nets)
 
 
@@ -175,10 +280,10 @@ def clean_cts():
 
 
 def clean_images():
-    images = DOCKER_CLIENT.images.list()
+    images = DOCKER_CLIENT.images.list(all=True)
     for image in images:
         try:
-            DOCKER_CLIENT.images.remove(image.id)
+            DOCKER_CLIENT.images.remove(image.id, force=True)
         except NotFound:
             pass
 
